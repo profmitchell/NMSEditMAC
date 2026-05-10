ca060e6605f67d83ebc314d988139941f3e8412c
Mitchell Cohen <122713703+profmitchell@users.noreply.github.com>
2026-05-10 18:38:10 -0400

    Initial Commit

---
+++ NMSEditMAC/NMSEditMACApp.swift
@@ -0,0 +1,32 @@
+//
+//  NMSEditMACApp.swift
+//  NMSEditMAC
+//
+//  Created by Mitchell Cohen on 5/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NMSEditMACApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
